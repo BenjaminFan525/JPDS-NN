fix numerical precision bugs of simulator

data/env_data_generetor.py
@@ -74,7 +74,7 @@ config = {
 }
 
 
-def data_gen(data_num, field_num, veh_num, task_size, save_dir, save_ia):
+def data_gen(data_num, field_num, veh_num, task_size, save_dir, save_ia, single_end):
     if not os.path.exists(save_dir):
         os.mkdir(save_dir)
     
@@ -129,12 +129,12 @@ def data_gen(data_num, field_num, veh_num, task_size, save_dir, save_ia):
                     field.merge_field([0, 1])
             else:
                 field = multiField(splits, type=field_type, width = (width, width), working_width=working_width,
-                                num_starts=veh_num, num_ends=veh_num) 
+                                num_starts=veh_num, num_ends=veh_num, single_end=single_end) 
                 if field_num == 3 or field_num == 5:
-                    field.merge_field([0, 1])
+                    field.merge_field([0, 1], num_starts=veh_num, num_ends=veh_num)
             
             line_nums = [f.num_working_lines for f in field.fields]
-            if np.all(np.array(line_nums) > 1) == False:
+            if np.all(np.array(line_nums) > 3) == False or np.sum(line_nums) < 20:
                 continue
             else:
                 break
@@ -200,7 +200,8 @@ if __name__ == "__main__":
     parser.add_argument('--data_num', type=int, default=10)
     parser.add_argument('--field_num', nargs='+', type=int, default=[5, 2, 3])
     parser.add_argument('--veh_num', nargs='+', type=int, default=[1, 2, 3])
-    parser.add_argument('--save_ia', action='store_true', default=True)
+    parser.add_argument('--save_ia', action='store_true', default=False)
+    parser.add_argument('--single_end', action='store_true', default=False)
     # parser.add_argument('--field_edge', type=float, default=)
     parser.add_argument('--task_size', type=str, default='s')
     parser.add_argument('--save_dir', type=str, default='/home/fanyx/mdvrp/data/Gdataset/Task_test_xxx')
@@ -209,6 +210,6 @@ if __name__ == "__main__":
 
     for f_num in args.field_num:
         for v_num in args.veh_num:
-            data_gen(args.data_num, f_num, v_num, args.task_size, args.save_dir, args.save_ia)
+            data_gen(args.data_num, f_num, v_num, args.task_size, args.save_dir, args.save_ia, args.single_end)
 
     

env/multi_field.py
@@ -22,7 +22,7 @@ class multiField():
 
     def __init__(self, num_splits, type = '#', width: Tuple = (200, 300), center_lat_lon = (40, 116), working_width = None, 
                  boundary_coords: Optional[np.ndarray] = None, starts: Optional[str] = None, ends: Optional[str] = None, 
-                 num_starts=None, num_ends=None, num_veh=None):
+                 num_starts=None, num_ends=None, single_end=False):
         for key, val in self.defauts.items():
             setattr(self, key, val)
         
@@ -128,6 +128,11 @@ class multiField():
         # sample starts and ends
         self.starts = self.sample_nodes(num_starts) if starts is None else starts
         self.ends = self.sample_nodes(num_ends) if ends is None else ends
+        if single_end:
+            end = self.sample_nodes(1)[0]
+            self.ends = [end for _ in range(num_ends)]
+        self.single_end = single_end
+        
         # TODO: change here
         self.num_veh = len(self.starts)
         self.num_endpoints = self.num_veh if self.ends is None else 2*self.num_veh
@@ -345,7 +350,7 @@ class multiField():
         return 0
         
     
-    def merge_field(self, merge_ids : list):
+    def merge_field(self, merge_ids : list, num_starts=None, num_ends=None):
         assert len(merge_ids) > 1, f"input must be looger than 2, got {len(merge_ids)}"
         row, col = self.num_splits[0] + 1, self.num_splits[1] + 1
         
@@ -388,6 +393,12 @@ class multiField():
                              dis_matrix=False, 
                              utm_base=self.utm_base)
                        for idx, data in enumerate(self.fields)]
+        
+        self.starts = self.sample_nodes(num_starts) if num_starts is not None else self.starts
+        self.ends = self.sample_nodes(num_ends) if num_ends is not None else self.ends
+        if self.single_end:
+            end = self.sample_nodes(1)[0]
+            self.ends = [end for _ in range(num_ends)]
         
         self.Graph: nx.Graph = nx.compose_all([self.Graph] + [field.Graph for field in self.fields])
 

env/simulator.py
@@ -170,7 +170,7 @@ class Simulator:
         self.lines_id = lines_id
         self.field = field
         # self.car_status = ['home', [0, 0]]*len(car_list)
-        self.car_status = [{'line': self.field.nodes_list[0], 'pos': path[0][0], 'inline': False} for _ in range(len(car_list))]
+        self.car_status = [{'line': self.field.nodes_list[0+idx], 'pos': path[0][0], 'inline': False} for idx, _ in enumerate(range(len(car_list)))]
         self.traveled_line = [[] for _ in range(len(car_list))]
         if car_tracking:
             self.car_tracking = car_tracking # 车辆是否跟踪路径
@@ -223,12 +223,12 @@ class Simulator:
                     car.stop()
                 # 经过了终点，换下一个路径
                 elif (car.position() - self.path_list[idx][1]) @ self.tracking_vector_list[idx] >= 0:
-                    if len(self.line_nodes[idx]) and np.array_equal(self.path_list[idx][0], self.line_nodes[idx][0]):
+                    if len(self.line_nodes[idx]) and np.allclose(self.path_list[idx][0], self.line_nodes[idx][0]):
                         self.line_nodes[idx] = np.delete(self.line_nodes[idx], 0, axis=0)
                         if len(self.line_nodes[idx]) % 2 == 0:
                             self.traveled_line[idx].append(self.field.working_line_list[self.lines_id[idx].pop(0)[0]])
                     
-                    self.car_status[idx]['line'] = self.field.working_line_list[self.lines_id[idx][0][0]] if len(self.lines_id[idx]) else self.field.nodes_list[0]
+                    self.car_status[idx]['line'] = self.field.working_line_list[self.lines_id[idx][0][0]] if len(self.lines_id[idx]) else self.field.nodes_list[idx+len(self.car_list)]
                     self.car_status[idx]['pos'] = self.path_list[idx][0]
                     self.car_status[idx]['inline'] = len(self.line_nodes[idx]) % 2 != 0
                     
@@ -279,10 +279,10 @@ class Simulator:
             ori_ax = len(ax.lines)
             if output_figure:
                 figures = []
-        # text = ax.text(0.01, 0.99, 'Initializing', 
-        #                horizontalalignment='left', 
-        #                verticalalignment='top',
-        #                transform=ax.transAxes)
+        text = ax.text(0.01, 0.99, 'Initializing', 
+                       horizontalalignment='left', 
+                       verticalalignment='top',
+                       transform=ax.transAxes)
         
         while not self.step(a):
             if render:
@@ -291,8 +291,8 @@ class Simulator:
                         while len(ax.lines) > ori_ax:
                             ax.lines.pop()
                     self.render(ax, show = False)
-                    # disp_str = ''.join([f"Car_{idx+1}:\t line: {status['line']}\t pos: {status['pos']}\t inline: {status['inline']}\n" for idx, status in enumerate(self.car_status)])
-                    # text.set_text(disp_str)
+                    disp_str = ''.join([f"Car {idx+1} | line {status['line']} | pos {status['pos']} | inline {status['inline']}\n" for idx, status in enumerate(self.car_status)])
+                    text.set_text(disp_str)
                     if output_figure:
                         canvas = FigureCanvasAgg(plt.gcf())
                         w, h = canvas.get_width_height()
@@ -305,7 +305,7 @@ class Simulator:
                         figures.append(np.asarray(image)[:, :, :3])
                     if show:
                         plt.pause(0.01)
-        
+
         return figures if render and output_figure else None
             
     def pause(self):

experiment/case_study/sim.py
@@ -1,27 +1,34 @@
-import env.dubins as dubins
-from shapely import geometry
-import geopandas
-import utm
-from geopandas import GeoSeries
-import pandas as pd
-
-from env.field import Field
-from env.car import car, Robot
 from env.multi_field import multiField
-from algo import MGGA
 from utils import fit, load_dict, decode
 import os
-from model.ac import load_model
-from copy import deepcopy
 from torch_geometric.data import Batch
 import torch
 from torch_geometric.utils import unbatch, from_networkx
-from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
-from env import arrangeSimulator, simulator
-import cv2
+from env import arrangeSimulator
+import json
+from model.ac import GNNAC
+import ia.algo.arrangement.models.ac as ia_model
+import ia.utils as ia_util
+import ia.env.simulator as ia_env
+from matplotlib.animation import FuncAnimation
 
+def load_model(checkpoint, model_type='mdvrp'):
+    cfg = os.path.join('/'.join(checkpoint.split('/')[:-1]), 'config.json')
+    with open(cfg, 'r') as f:
+        cfg = json.load(f)
+    
+    ac_cfg = cfg['ac']
+
+    if model_type == 'mdvrp':
+        ac = GNNAC(**ac_cfg)
+    elif model_type == 'ia':
+        ac = ia_model.GNNAC(**ac_cfg)
+    checkpoint = torch.load(checkpoint, map_location='cpu')
+    ac.load_state_dict(checkpoint['model'])  
+    ac.eval()
+    return ac
 
 data = "/home/fanyx/mdvrp/experiment/case_study/4_4/0_39_24"
 field_ia = load_dict(os.path.join(data, 'field.pkl'))
@@ -40,13 +47,12 @@ car_cfg_0 = [{'vw': 2.5, 'vv': 4.5, 'cw': 0.008, 'cv': 0.006, 'tt': 0, 'min_R': 
 algo = 'PPO-t'
 f = 't'
 
-# checkpoint = "/data/fanyx_files/Intelligent-Agriculture/ia/algo/arrangement/runs_rl/ppo/2024-03-04__12-53_t/best_model39.pt"
+checkpoint_ia = "/home/fanyx/Intelligent-Agriculture/ia/algo/arrangement/runs_rl/ppo/2024-03-04__12-53_t/best_model39.pt"
 checkpoint = "/home/fanyx/mdvrp/result/training_rl/ppo/2024-12-27__12-11/best_model31.pt"
 
-car_cfg_v = [[cur_car['vw'], cur_car['vv'],
+car_tensor = torch.tensor(np.array([[cur_car['vw'], cur_car['vv'],
                 cur_car['cw'], cur_car['cv'],
-                cur_car['tt']] for cur_car in car_cfg]
-car_tensor = torch.tensor(np.array(car_cfg_v)).float()
+                cur_car['tt']] for cur_car in car_cfg])).float()
 
 field = multiField(num_splits=[1, 1], 
                        starts=['bound-2-1', 
@@ -64,13 +70,10 @@ field = multiField(num_splits=[1, 1],
 field.from_ia(field_ia)
 # field_list = [0, 3]
 # field.make_working_graph(field_list)
-field.make_working_graph()
+# field.make_working_graph()
 
 ac = load_model(checkpoint)
-ac.num_veh = field.num_veh
 ac.sequential_sel = True
-ac.end_en = True if field.ends is not None else False
-ac.encoder.end_en = ac.end_en
 
 pygdata = from_networkx(field.working_graph, group_node_attrs=['embed'], group_edge_attrs=['edge_embed'])
 data_t = {'graph': Batch.from_data_list([pygdata]), 'vehicles': car_tensor.unsqueeze(0)}
@@ -78,40 +81,45 @@ info = {'veh_key_padding_mask': torch.zeros((1, len(car_tensor))).bool(), 'num_v
 
 with torch.no_grad():
     seq_enc, _, _, _, _, _, _ = ac(data_t, info, deterministic=True, criticize=False, )
-
 T = decode(seq_enc[0])
 
-figure, ax = plt.subplots()
-ax.axis('equal')
-name = 'dijkstra_path.png'
-path = os.path.join(data, name)
-sim = arrangeSimulator(field, car_cfg_0)
-sim.init_simulation(T, init_simulator=False)
-sim.render_arrange(ax)
-plt.savefig(path)
+field_ia = load_dict(os.path.join(data, 'field.pkl'))
+ac_ia = load_model(checkpoint_ia, model_type='ia')
+pygdata = from_networkx(field_ia.working_graph, group_node_attrs=['embed'], group_edge_attrs=['edge_embed'])
+data_t = {'graph': Batch.from_data_list([pygdata]), 'vehicles': car_tensor.unsqueeze(0)}
+info = {'veh_key_padding_mask': torch.zeros((1, len(car_tensor))).bool(), 'num_veh': torch.tensor([[car_tensor.shape[0]]])}
+with torch.no_grad():
+    seq_enc, _, _, _, _, _, _ = ac_ia(data_t, info, deterministic=True, criticize=False, )
 
-print("Initializing")
-simulator = arrangeSimulator(field, car_cfg)
-simulator.init_simulation(T, debug=True)
-simulator.render_arrange()
-name = 'dubins_path.png'
-path = os.path.join(data, name)
-plt.savefig(path)
-# cars = [Robot({'car_model': "car3", 'working_width': working_width, **cfg}, 
-#              state_ini=[path[0, 0], path[0, 1], 0, 0, f_dir, 0], debug=True) 
-#         for path, f_dir, cfg in zip(paths, first_dir, car_cfg)]
-# simulator = Simulator(paths, cars, working_list, max_step=500000)
-# simulator.set_drive_mode("direct")#physical
-print("Start simulation")
-t, c, s, car_time, car_dis, figs = simulator.simulate(True, True, True)
-# plt.show()
-name = 'working_result.png'
-path = os.path.join(data, name)
-plt.savefig(path)
-print(path)
+T_ia = ia_util.decode(seq_enc[0])
 
-chosen_idx = [[line[0] for line in simulator.simulator.traveled_line[idx]] for idx in range(len(car_cfg))]
-chosen_entry = [[line[1] for line in simulator.simulator.traveled_line[idx]] for idx in range(len(car_cfg))]
+for idx, T in enumerate([T, T_ia]):
+    print(f"Simulator is initialing...")
+    simulator = arrangeSimulator(field, car_cfg)
+    simulator.init_simulation(T, debug=True)
+    simulator.render_arrange()
+
+    a = np.array([[cfg['vw'], cfg['vv']] for cfg in simulator.car_cfg])
+
+    t, c, s, car_time, car_dis, figs = simulator.simulate(True, True, False)
+
+    s, t, c = fit(field.D_matrix, 
+                    field.ori, 
+                    field.des,
+                    car_cfg, 
+                    field.line_length,
+                    T, 
+                    tS_t=False,
+                    type='all')
+    plt.title(f'$s_P$={np.round(s, 2)}m, $t_P$={np.round(t, 2)}s, $c_P$={np.round(c, 2)}L', fontsize=20)
+    print(f"Simulation result: s={np.round(s, 2)}m, t={np.round(t, 2)}s, c={np.round(c, 2)}L")
+    
+
+plt.show()
+
+
+# chosen_idx = [[line[0] for line in simulator.simulator.traveled_line[idx]] for idx in range(len(car_cfg))]
+# chosen_entry = [[line[1] for line in simulator.simulator.traveled_line[idx]] for idx in range(len(car_cfg))]
 
 # if figs is not None:
 #     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
@@ -124,16 +132,3 @@ chosen_entry = [[line[1] for line in simulator.simulator.traveled_line[idx]] for
 #     print('error')
     
 # print(os.path.join(data, algo + ".mp4"))
-
-print(f'Distance:{s} Time:{t} Fuel:{c}')
-
-s, t, c = fit(field.D_matrix, 
-                    field.ori, 
-                    field.des,
-                    car_cfg, 
-                    field.line_length,
-                    T, 
-                    tS_t=False,
-                    type='all')
-
-print(f'Distance:{s} Time:{t} Fuel:{c}')